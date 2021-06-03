b78a74f5157f21ef5abb38721005927f1d16282c
JohnHeo04 <hjo199456@gmail.com>
2021-06-03 19:15:27 +0900

    32.  Disable user Interactions with text fields : 편집상태일 때의 조건들과 아닐 때의 조건 생성, 계속해서 났던 Round 주는 버그와 'Fatal Error' 해결

--- LetsMeet/ProfileTableViewController.swift
+++ LetsMeet/ProfileTableViewController.swift
@@ -27,13 +27,30 @@ class ProfileTableViewController: UITableViewController {
     @IBOutlet weak var heightTextField: UITextField!
     @IBOutlet weak var lookingForTextField: UITextField!
     
+    //MARK: - Vars
+    // 편집모드가 아닐때는 편집 불가 상태로 바꾸는 변수
+    var editingMode = false
+    
+    
+    
     //MARK: - ViewLifeCycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 31.프로필화면의 Round기능이 버그인가 했지만 아래의 메소드를 활성화 시켜야 Round가 적용되는 것을 찾음
+        overrideUserInterfaceStyle = .light
+        
+        setupBackgrounds()
+        updateEditingMode()
         
     }
+    //  프로필에 있는 섹션들을 없앰
+    //  return 0으로 반환하여 각 테이블에 있는 Section을 없애줌
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+    
     
     
     //MARK: - IBActions
@@ -47,6 +64,16 @@ class ProfileTableViewController: UITableViewController {
     }
     
     @IBAction func editButtonPressed(_ sender: Any) {
+        // toggle()은 on or off 스위치 형태로 켰다 껐다 해주는 기능
+        editingMode.toggle()
+        updateEditingMode()
+        // 아래는 if문과 비슷함 - editingMode가 true라면 showKeyboard() 활성, : false라면 hideKeyboard() 활성
+        editingMode ? showKeyboard() : hideKeyboard()
+        showSaveButton()
+    }
+    
+    @objc func editUserData() {
+        
         
     }
     
@@ -56,16 +83,40 @@ class ProfileTableViewController: UITableViewController {
         profileCellBackgroundView.clipsToBounds = true
         profileCellBackgroundView.layer.cornerRadius = 100
         profileCellBackgroundView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-
+        //
         aboutMeTextView.layer.cornerRadius = 10
         
     }
-    
-    //  프로필에 있는 섹션들을 없앰
-    //  return 0으로 반환하여 각 테이블에 있는 Section을 없애줌
-    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 0
+    // 'Save' 버튼 만드는 함수
+    // 만약 편집 모드 라면 saveButton이 생기고, 아니라면 사라짐
+    private func showSaveButton() {
+        
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(editUserData))
+        
+        navigationItem.rightBarButtonItem = editingMode ? saveButton : nil
     }
     
+    //MARK: - Editing Mode
+    private func updateEditingMode() {
+        // 아래 height과 lookingFor에 계속 'Fatal Error : Thread 1'오류가 나서 계속 방황하다가 윗쪽에서 'Storyboard'와 위쪽 변수명이 끊긴걸 발견하고 다시 이어줌 😤
+        aboutMeTextView.isUserInteractionEnabled = editingMode
+        jobTextField.isUserInteractionEnabled = editingMode
+        educationTextField.isUserInteractionEnabled = editingMode
+        genderTextField.isUserInteractionEnabled = editingMode
+        cityTextField.isUserInteractionEnabled = editingMode
+        countryTextField.isUserInteractionEnabled = editingMode
+        heightTextField.isUserInteractionEnabled = editingMode
+        lookingForTextField.isUserInteractionEnabled = editingMode
+    }
+    
+    
+    //MARK: - Helpers
+    private func showKeyboard() {
+        self.aboutMeTextView.becomeFirstResponder()
+    }
+    
+    private func hideKeyboard() {
+        self.view.endEditing(false)
+    }
     
 }
