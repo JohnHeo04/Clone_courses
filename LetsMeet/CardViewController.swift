05113a5531db0c422a3a650685610f673db959e8
JohnHeo04 <hjo199456@gmail.com>
2021-06-14 13:35:52 +0900

    ### Section 4: Card View Controller 58. Card View controller introduction : - 로그인 - 프로필 화면 구현 및 함수 구현 끝내고 - 카드뷰 섹션으로 넘어옴 	+ 이 섹션에서는 mac-gallagher / Shuffle 라이브러리를 쓸 예정

---
+++ LetsMeet/CardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CardViewController.swift
+//  LetsMeet
+//
+//  Created by John Hur on 2021/06/14.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
