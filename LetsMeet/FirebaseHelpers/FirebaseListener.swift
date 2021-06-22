ac73ca61e60e2582fa54796603bc7865976fff9d
JohnHeo04 <hjo199456@gmail.com>
2021-06-22 12:59:15 +0900

    68. Download Users from firebase Part 2 : - 파이어베이스로 부터 사용자 정보를 불러들이는 함수 구현 	+ DB 사용자 사진 다운받는 쿼리 작성

--- LetsMeet/FirebaseHelpers/FirebaseListener.swift
+++ LetsMeet/FirebaseHelpers/FirebaseListener.swift
@@ -50,9 +50,62 @@ class FirebaseListener {
             
         }
     }
-    
+    // 사용자 사진을 다운받기 위한 쿼리를 짜는 함수 구현
     func downloadUserFromFirebase(isInitialLoad: Bool, limit: Int, lastDocumentSnapshot: DocumentSnapshot?, completion: @escaping (_ users: [FUser], _ snapshot: DocumentSnapshot?) -> Void ) {
-     
+        
+        var query: Query!
+        var users: [FUser] = []
+        
+        if isInitialLoad {
+            query = FirebaseReference(.User).order(by: kREGISTEREDDATE, descending:  false).limit(to: limit)
+            print("first \(limit) users loading")
+            
+            
+        } else {
+            
+            if lastDocumentSnapshot != nil {
+                query = FirebaseReference(.User).order(by: kREGISTEREDDATE, descending:  false).limit(to: limit).start(afterDocument: lastDocumentSnapshot!)
+                
+                print("next \(limit) user loading")
+                
+            } else {
+                print("last snapshot is nil")
+                
+            }
+        }
+        
+        if query != nil {
+            
+            query.getDocuments { (snapShot, error) in
+                
+                guard let snapshot = snapShot else { return }
+                
+                if !snapshot.isEmpty {
+                    
+                    for userData in snapshot.documents {
+                        
+                        let userObject = userData.data() as NSDictionary
+                        
+                        if !(FUser.currentUser()?.likedIdArray?.contains(userObject[kOBJECTID] as! String) ?? false) && FUser.currentId() != userObject[kOBJECTID] as! String {
+                            
+                            users.append(FUser(_dictionary: userObject))
+                        }
+                    }
+                    
+                    completion(users, snapshot.documents.last!)
+                    
+                } else {
+                    print("no more users to fetch")
+                    completion(users, nil)
+                    
+                }
+                
+            }
+            
+        } else {
+            completion(users, nil)
+        }
+        
      
     }
     
